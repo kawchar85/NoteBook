7c1f72c036570575444fdfcb41db1c2b4e268084
Kawchar Husain <52884694+kawchar85@users.noreply.github.com>
2022-10-04 22:39:26 +0600

    Create Aliens Trick.cpp

---
+++ Dynamin Programming/Aliens Trick.cpp
@@ -0,0 +1,17 @@
+theorem:area=In+(on_boundary/2)-1;in=strictly inside polygon
+pair<ll,ll> solveWithPenalty(ll p){
+  //add p with targets
+  ll ans=0,cnt=0;
+  //cal ans
+  //remove p from targets
+  return {ans, cnt}; }
+ll Alien(ll l, ll r, ll need){
+  ll ans=-1;
+  while(l <= r) {
+    ll mid = l+(r-l)/2;
+    //solveWithPenalty() retrun {ansWithPenalty, cnt}
+    pair<ll,ll> res = solveWithPenalty(mid); 
+    if(res.second >= need) 
+      ans = res.first - mid * need, l = mid + 1;
+    else r = mid - 1;
+  }  return ans; }
