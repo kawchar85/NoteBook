e56792585c9967f0bcd4af67a05f74fb678a01cb
Kawchar Husain <52884694+kawchar85@users.noreply.github.com>
2022-10-04 22:06:34 +0600

    Update Nim Game.cpp

--- Game/Nim Game.cpp
+++ Game/Nim Game.cpp
@@ -1,3 +1,5 @@
-Nim Game: Lose=who can't make a move if(xorSum != 0) 1 win;
+Nim Game: Lose=who can't make a move; if(xorSum != 0) 1 win;
 Misera Nim: Lose = who make the last move
 if all 1: if N is even, 1 win; else: if xorSum != 0, 1 win
+Composite: Sob pile er jonno Grundy ber kore Nim er moto xor
+Stones: N=> sub 2/3/5; if n%7 > 1, 1 win ;;
