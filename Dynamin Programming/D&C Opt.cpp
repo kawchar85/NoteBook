be93c547ed400e3c154c91b0786b92b7e90cd5d6
Kawchar Husain <52884694+kawchar85@users.noreply.github.com>
2022-10-04 22:44:48 +0600

    Create D&C Opt.cpp

---
+++ Dynamin Programming/D&C Opt.cpp
@@ -0,0 +1,14 @@
+ll dp[N]; ll cost(int l, int r){ return 1; } //dummy
+void solve(int l, int r, int ql, int qr) {
+  if(l > r) return;  int mid = l + (r - l) / 2;
+  ll pos =0, val = INT_MAX; //check min or max ?
+  for(int k = ql; k <= min(mid, qr); k++) {
+    ll ret = cost(k, mid);
+    if(ret < val) val = ret, pos = k; /*min or max? */  }
+  dp[mid] = val;
+  // for H[j]<=H[j+1] i.e. cost(1,j)<=cost(1,j+1)
+  solve(l, mid - 1, ql, pos);
+  solve(mid + 1, r, pos, qr);
+  /* for H[j]>=H[j+1] i.e. cost(1,j)>=cost(1,j+1)
+  solve(l, mid - 1, pos, qr);
+  solve(mid + 1, r, ql, pos); */    }
