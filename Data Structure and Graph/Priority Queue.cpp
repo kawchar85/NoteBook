d0abbfbc5a998ef95f7c470e31666f5ec4b256ef
Kawchar Husain <52884694+kawchar85@users.noreply.github.com>
2023-07-17 00:46:44 +0600

    Delete Priority Queue.cpp

--- Data Structure and Graph/Priority Queue.cpp
+++
@@ -1,5 +0,0 @@
-struct cmp{
-  bool operator() (segment a, segment b) const{
-    return (a.len>b.len)||((a.len==b.len) && a.l<b.l); } };
-/* bigger len will be first and if lens are equal 
-then less l will be first *///////////////////
