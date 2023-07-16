c7d556e980d40462a1c351565813af4b1011553a
Kawchar Husain <52884694+kawchar85@users.noreply.github.com>
2023-07-17 01:21:34 +0600

    Update Xor Basis.cpp

--- Data Structure and Graph/Xor Basis.cpp
+++ Data Structure and Graph/Xor Basis.cpp
@@ -5,7 +5,7 @@ struct XorBasis{
     for(auto &i : basis) x=min(x,x^i);
     if(!x) return;
     for(auto &i : basis) if((i^x)<i) i^=x;
-    basis.push_back(x); sort(basis.begin(), basis.end());}
+    basis.push_back(x); sort(basis.begin(),basis.end());}
   ll size(){ return (ll)basis.size(); }
   void clear(){ N=0;tmp=0; basis.clear(); }
   bool possible(ll x){
@@ -15,7 +15,7 @@ struct XorBasis{
   ll minxor(ll x=0){
     for(auto &i: basis) x=min(x, x^i); return x; }
   ll cntxor(ll x){
-    if(!possible(x)) return 0LL; return (1LL<<(N-size())); }
+    if(!possible(x)) return 0; return (1LL<<(N-size()));}
   ll sumOfAll(){
     ll ans=tmp*(1LL<<(N-1)); return ans; }
   ll kth(ll k){
@@ -23,3 +23,15 @@ struct XorBasis{
     k--; ll ans=0;
     for(ll i=0; i<sz; i++) if(k>>i & 1) ans^=basis[i];
     return ans;  }  }xb;
+////V2
+int basis[LOG_K], sz;
+void insertVector(int mask) {
+  for (int i = LOG_K - 1; i >= 0; i--) {
+    if ((mask & 1LL << i) == 0) continue;
+    if (!basis[i]) { basis[i] = mask; sz++; return;}
+    mask ^= basis[i]; } }
+int query(int k) { int ans = 0;
+  for(int i = LOG_K - 1; i >= 0; i--){
+    if(k & (1LL << i)){
+      if(!basis[i]) return -1;
+       ans++, k ^= basis[i]; } } return ans; }
