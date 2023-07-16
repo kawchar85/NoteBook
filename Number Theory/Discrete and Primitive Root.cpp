c0917153be62c8817ef5423efd383d7cb6cec1d3
Kawchar Husain <52884694+kawchar85@users.noreply.github.com>
2023-07-17 02:50:24 +0600

    Update Discrete and Primitive Root.cpp

--- Number Theory/Discrete and Primitive Root.cpp
+++ Number Theory/Discrete and Primitive Root.cpp
@@ -1,5 +1,5 @@
 /*Finds the primitive root modulo p. g is a primitive root 
-modulo p if and only if for any integer a such that gcd(a,p)
+mod p if and only if for any integer a such that gcd(a,p)
 =1, there exists an integer k such that: g^k = a(mod p)*/
 int PrimitiveRoot (int p) {
   vector<int> fact;
@@ -7,7 +7,7 @@ int PrimitiveRoot (int p) {
   int n = phi;
   for(int i=2; i*i<=n; ++i){
     if(n%i == 0){
-        fact.push_back (i);  while(n % i == 0)  n /= i; }  }
+      fact.push_back (i);  while(n % i == 0)  n /= i; }}
   if(n>1) fact.push_back (n);
   for(int res=2; res<=p; ++res){
     bool ok = true;
@@ -17,22 +17,22 @@ int PrimitiveRoot (int p) {
   }  return -1;  }
 //find all numbers x such that x^k = a (mod n)
 void printDiscreteRoot(int k, int a, int n){
-	if(a==0){	cout<<"1\n0\n"; return;  }
- 	int g=PrimitiveRoot(n);
- 	int phi=n-1; //if n is not a prime calculate phi(n)
- 	int sq=(int) sqrt(n+0.0)+1;
-	vector<pair<int,int> > dec (sq);
-	for(int i=1; i<=sq; ++i)
-		dec[i-1]={BigMod(g, 1LL*i*sq%phi*k%phi,n), i};
- 	sort(dec.begin(), dec.end());
- 	int any_ans = -1;
- 	for(int i=0; i<sq; ++i) {
- 		int my=BigMod(g, 1LL*i*k%phi, n)*1LL*a%n;
+  if(a==0){ cout<<"1\n0\n"; return;  }
+  int g=PrimitiveRoot(n);
+  int phi=n-1; //if n is not a prime calculate phi(n)
+  int sq=(int) sqrt(n+0.0)+1;
+  vector<pair<int,int> > dec (sq);
+  for(int i=1; i<=sq; ++i)
+    dec[i-1]={BigMod(g, 1LL*i*sq%phi*k%phi,n), i};
+  sort(dec.begin(), dec.end());
+  int any_ans = -1;
+  for(int i=0; i<sq; ++i) {
+    int my=BigMod(g, 1LL*i*k%phi, n)*1LL*a%n;
  auto it=lower_bound(dec.begin(),dec.end(),make_pair(my,0));
-	 	if(it != dec.end() && it->first == my){
- 			any_ans = it->second * sq - i;	break;	} 	}
- 	if(any_ans==-1){	cout<<"0\n"; return ; 	}
- 	// all possible answers
+    if(it != dec.end() && it->first == my){
+      any_ans = it->second * sq - i;  break;  }   }
+  if(any_ans==-1){  cout<<"0\n"; return ;   }
+  // all possible answers
   int delta = (n-1) / __gcd(k, n-1);
   vector<int> ans;
   for(int cur=any_ans%delta; cur<n-1; cur+=delta)
