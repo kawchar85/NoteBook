61020b96f7726154916fff2ceb6f26cb486a4051
Kawchar Husain <52884694+kawchar85@users.noreply.github.com>
2023-07-17 02:45:41 +0600

    Update BigMod.cpp

--- Number Theory/BigMod.cpp
+++ Number Theory/BigMod.cpp
@@ -1,6 +1,11 @@
 ll BigMod(ll a,ll p, ll M=MOD){
-    if(!p) return 1%M;
-    ll x=Biod(a,p/2,M);
-    x=(x*x)%M;//Overflow?
-    if(p&1) x=(x*a)%M;
-    return x;   }
+  if(!p) return 1%M;
+  ll x=Biod(a,p/2,M); x=(x*x)%M;
+  if(p&1) x=(x*a)%M; return x; }
+
+ll Big_Mod(ll a, ll p, ll M=MOD){
+  ll result=1;
+  while(p>0){ if(p&1){
+      result*=a; if(result>M) result%=M; }
+    p>>=1; a*=a; if(a>M) a%=M;
+  } return result%M; }
