b09b67d1181f99eb4af00477165341c247c96355
Kawchar Husain <52884694+kawchar85@users.noreply.github.com>
2023-07-17 01:41:35 +0600

    Update preCode.cpp

--- Random/preCode.cpp
+++ Random/preCode.cpp
@@ -1,7 +1,21 @@
-ios_base::sync_with_stdio(0); cin.tie(0);
+#include<bits/stdc++.h>
+using namespace std;
 #define ALL(v) v.begin(),v.end()
-#define Unique(a) sort(ALL(a)); (a).resize(unique(ALL(a)) - (a).begin())
-iint x4[]={+1,-1,+0,+0}; int y4[]={+0,+0,+1,-1};
+#define clean(x,y) memset(x,y,sizeof(x));
+#define endl "\n"
+#define MOD 1000000007
+#define MAX 200005
+typedef long long ll;
+int32_t main() {
+  ios_base::sync_with_stdio(false); cin.tie(NULL);
+  int TC = 1;
+  //cin>>TC;
+  for(int cs = 1; cs <= TC; cs++) {
+    //cout << "Case " << cs << ": ";
+    solve();
+  } return 0; }
+
+int x4[]={+1,-1,+0,+0}; int y4[]={+0,+0,+1,-1};
 int x8[]={+0,+0,+1,-1,-1,+1,-1,+1}; 
 int y8[]={-1,+1,+0,+0,+1,+1,-1,-1};
 int xk[]={1,1,2,2,-1,-1,-2,-2}; 
