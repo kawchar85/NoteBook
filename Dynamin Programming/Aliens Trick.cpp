3008f4b791b8324636ca5858a39c4d2e6e972dfd
Kawchar Husain <52884694+kawchar85@users.noreply.github.com>
2023-10-31 22:53:16 +0600

    Update Aliens Trick.cpp

--- Dynamin Programming/Aliens Trick.cpp
+++ Dynamin Programming/Aliens Trick.cpp
@@ -1,16 +1,12 @@
 pair<ll,ll> solveWithPenalty(ll p){
-  //add p with targets
   ll ans=0,cnt=0;
-  //cal ans
-  //remove p from targets
+  //add p with targets//cal ans//remove p from targets
   return {ans, cnt}; }
 ll Alien(ll l, ll r, ll need){
   ll ans=-1;
-  while(l <= r) {
-    ll mid = l+(r-l)/2;
+  while(l <= r) { ll mid = l+(r-l)/2;
     //solveWithPenalty() retrun {ansWithPenalty, cnt}
     pair<ll,ll> res = solveWithPenalty(mid); 
     if(res.second >= need) 
       ans = res.first - mid * need, l = mid + 1;
-    else r = mid - 1;
-  }  return ans; }
+    else r = mid - 1; }  return ans; }
