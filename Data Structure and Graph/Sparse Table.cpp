c8fe6aeb3027579216793a91207e73880e346afa
Kawchar Husain <52884694+kawchar85@users.noreply.github.com>
2024-01-22 00:40:40 +0600

    Update Sparse Table.cpp

--- Data Structure and Graph/Sparse Table.cpp
+++ Data Structure and Graph/Sparse Table.cpp
@@ -10,26 +10,32 @@ void build(int n){
 int query(int l, int r){
   int k=31-__builtin_clz(r-l+1);
   return merge(dst[l][k], dst[r-(1<<k)+1][k]);  }
-//2D Sparse Table: O(n^2 (logn)^2
-int A[MAXN][MAXN]; int M[MAXN][MAXN][LOGN][LOGN];
-void Build2DSparse(int N){ //1 based
-  for(int i = 1; i <= N; i++){
-    for(int j = 1; j <= N; j++)
-        M[i][j][0][0] = A[i][j];
-    for(int q = 1; (1<<q) <= N; q++){
-      int add = 1<<(q-1);
-      for(int j=1; j+add <= N; j++){
-        M[i][j][0][q] = merge(M[i][j][0][q-1], M[i][j+add][0][q-1]);  }  }  }
-    for(int p=1; (1<<p)<=N; p++){
-      int add = 1<<(p-1);
-      for(int i=1; i+add <= N; i++){
-        for(int q=0; (1<<q) <= N; q++){
-          for(int j=1; j<= N; j++)
-            M[i][j][p][q] = merge(M[i][j][p-1][q], M[i+add][j][p-1][q]);  } }  } }
-//merge() of all A[i][j], where x1<=i<=x2 and y1<=j<=y2
-int Query(int x1,int y1,int x2,int y2){
-  int kX = log2(x2-x1+1); int kY = log2(y2-y1+1);
-  int addX = 1<<kX;  int addY = 1<<kY;
-  int ret1 = merge(M[x1][y1][kX][kY], M[x1][y2-addY+1][kX][kY]);
-  int ret2 = merge(M[x2-addX+1][y1][kX][kY], M[x2-addX+1][y2-addY+1][kX][kY]);
-  return max(ret1, ret2); }
+vector<vector<vector<int>>> st;
+int merge(int a,int b,int c,int d) {
+    return min({a, b, c, d}); }
+//O(n*m*Log(max(n,m)) ///0-based
+void build2DSparseTable(int n,int m){
+  int LOG = log2(max(n, m))+1;
+  vector<vector<vector<int>>>().swap(st);
+  st.resize(n, vector<vector<int>>(m, vector<int>(LOG)));
+  for(int i = 0; i < n; i++)
+    for(int j = 0; j < m; j++)
+      st[i][j][0] = arr[i][j];
+  for(int k = 1; k < LOG; k++) {
+  for(int i = 0; i + (1<<k) <= n; i++) {
+  for(int j = 0; j + (1<<k) <= m; j++) {
+    st[i][j][k] = merge(
+      st[i][j][k-1],
+      st[i+(1<<(k-1))][j][k-1],
+      st[i][j+(1<<(k-1))][k-1],
+      st[i+(1<<(k-1))][j+(1<<(k-1))][k-1]
+    ); } } } }
+//O(merge())
+int query(int x1,int y1,int x2,int y2) {
+  int k = log2(min(x2-x1+1, y2-y1+1))+1;
+  return merge(
+    st[x1][y1][k-1],
+    st[x2-(1<<(k-1))+1][y1][k-1],
+    st[x1][y2-(1<<(k-1))+1][k-1],
+    st[x2-(1<<(k-1))+1][y2-(1<<(k-1))+1][k-1]
+    ); }
